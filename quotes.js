821b25b4a3e288a57d5123ed77d7c3f82fbb706c
Ben Gergler <bgergler@gmail.com>
2016-10-20 10:19:56 -0400

    derp

--- quotes.js
+++ quotes.js
@@ -1,27 +1,27 @@
-<script>
+<script LANGUAGE="JavaScript">
 var quotes = [
 	{
-	 "quote":"In order to succeed, we must first believe that we can."
+	 "quote":"In order to succeed, we must first believe that we can.",
 	 "author":"Nikos Kazantzakis"
 	},
 	{
-	 "quote":"It does not matter how slowly you go as long as you do not stop."
+	 "quote":"It does not matter how slowly you go as long as you do not stop.",
 	 "author":"Confucious"
 	},
 	{
-	 "quote":"Keep your eyes on the stars, and your feet on the ground."
+	 "quote":"Keep your eyes on the stars, and your feet on the ground.",
 	 "author":"Theodore Roosevelt"
 	},
 	{
-	 "quote":"Always do your best. What you plant now, you will harvest later."
+	 "quote":"Always do your best. What you plant now, you will harvest later.",
 	 "author":"Og Mandino"
 	},
 	{
-	 "quote":"It always seems impossible until its done."
+	 "quote":"It always seems impossible until its done.",
 	 "author":"Nelson Mandela"
 	},
 	{
-	 "quote":"A creative man is motivated by the desire to achieve, not by the desire to beat others"
+	 "quote":"A creative man is motivated by the desire to achieve, not by the desire to beat others",
 	 "author":"Ayn Rand"
 	}
 ];
