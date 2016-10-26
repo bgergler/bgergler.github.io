8df91227c8004b2167c0087d6b0caaf80dd8c57c
Ben Gergler <bgergler@gmail.com>
2016-10-26 17:04:41 -0400

    y

--- twitch.js
+++ twitch.js
@@ -1,4 +1,3 @@
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 $( document ).ready(fucntion() {
 	$.ajax({
 		url: 'https://wind-bow.hyperdev.space/twitch-api/streams/freecodecamp?callback=?',
