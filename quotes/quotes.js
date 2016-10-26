26dcdd9b59d5b46436c6281c52ae48cdb3cbf8c4
Ben Gergler <bgergler@gmail.com>
2016-10-26 19:44:33 -0400

    add quotes

--- quotes/quotes.js
+++ quotes/quotes.js
@@ -1,4 +1,39 @@
 $( document ).ready(function(){
+	var quotes = [
+	{
+	 "quote":"In order to succeed, we must first believe that we can.",
+	 "author":"Nikos Kazantzakis"
+	},
+	{
+	 "quote":"It does not matter how slowly you go as long as you do not stop.",
+	 "author":"Confucious"
+	},
+	{
+	 "quote":"Keep your eyes on the stars, and your feet on the ground.",
+	 "author":"Theodore Roosevelt"
+	},
+	{
+	 "quote":"Always do your best. What you plant now, you will harvest later.",
+	 "author":"Og Mandino"
+	},
+	{
+	 "quote":"It always seems impossible until its done.",
+	 "author":"Nelson Mandela"
+	},
+	{
+	 "quote":"A creative man is motivated by the desire to achieve, not by the desire to beat others",
+	 "author":"Ayn Rand"
+	},
+	{
+	 "quote":"Life is 10% what happens to you and 90% how you react to it.",
+	 "author":"Charles R. Swindoll"
+	 },
+	 {
+		 "quote":"Make your life a masterpiece; imagine no limitations on what you can be, have, or do.",
+		 "author":"Brian Tracy"
+	 }
+];
+
 //choose a random number between 0 and size of quotes array
   var quoteIndex = Math.floor((Math.random()*quotes.length));
   var splicedQuotes = [];
