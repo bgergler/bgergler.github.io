3537a07c65584e1fa791561317bbb280f0a6ecb9
Ben Gergler <bgergler@gmail.com>
2017-06-02 16:54:08 -0400

    Controls
    Began adding controls

--- twitch/twitch.js
+++ twitch/twitch.js
@@ -2,4 +2,20 @@ $(document).ready(function() {
 $.getJSON("TwitchAPIResults.json", function(json) {
 $(".message").html(JSON.stringify(json));
 });
+var activeDisplay = displayAll;
+      $('.displayOptions').bind('mouseenter', 
+        function(event){ 
+           $(event.target).css('width', '50px');
+      });
+      $('.displayOptions').bind('mouseleave', 
+        function(event){ 
+           $(event.target).css('width', '10px');
+      });
+      $('.displayOptions').click(function(event){
+      	if(event.target != activeDisplay){
+      	$(activeDisplay).css('width', '10px;');
+      	$(event.target).css('width', '50px');
+}
+      })
+
 });
